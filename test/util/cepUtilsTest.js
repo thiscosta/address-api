17cfe0ed1afee81f155d135abd8e56042ca09213
Thiscosta <thiscosta@live.com>
2020-09-16 20:24:59 -0300

    Added test dependencies and created test for CEP char replace utility method

---
+++ test/util/cepUtilsTest.js
@@ -0,0 +1,13 @@
+import assert from "assert";
+
+import { replaceCepDigit } from "../../src/utils/cepUtils.js";
+
+it("Should return the correct changed cep", function () {
+  const cep = "22333999";
+  const expectedCep = "22333990";
+  assert.strictEqual(
+    replaceCepDigit(cep),
+    expectedCep,
+    "The utility method for change last valid digit from CEP did not worked as expected"
+  );
+});
